cd0e6796a6484ffcf72412d52ffb441361b26b97
Omar Guaraca <omarguaraca51@gmail.com>
2019-07-19 02:55:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var name = "Omar"
-var height = "74"
+var height = "84"
 
 console.log('Name:');
 console.log(name);
 
 console.log('Height:');
-console.log(height);
+console.log(parseInt(height)+1);
 
 
 // Don't worry about this
